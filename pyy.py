4f713afdb69d0011fd0508fc16e6691d97178fef
Anjali punsi <108992453+anjipunsi@users.noreply.github.com>
2024-08-16 13:22:42 +0530

    pyy.py

--- pyy.py
+++ pyy.py
@@ -1,22 +1,20 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
 url = 'https://screener.in/company/RELIANCE/consolidated/'
 webpage = requests.get(url)
 soup = bs(webpage.text,'html.parser')
-
 data = soup.find('section', id="profit-loss")
 tdata= data.find("table")
-
 table_data = []
-
 for row in tdata.find_all('tr'):
     row_data = []
     for cell in row.find_all(['th','td']):
         row_data.append(cell.text.strip())
     table_data.append(row_data)
-
 
 df_table = pd.DataFrame(table_data)
 df_table.iloc[0,0] = 'Section'
@@ -27,11 +25,6 @@ for i in df_table.iloc[:,1:].columns:
 # df_table = df_table.set_index('')
 print(df_table)
 
-
-
-
-import psycopg2
-from sqlalchemy import create_engine
 db_host = "192.168.3.38" 
 db_name = "postgres"
 db_user = "scrap"
