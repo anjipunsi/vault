5311a8b17dfd1e260cb7d683f8664399af8e63c4
Anjali punsi <108992453+anjipunsi@users.noreply.github.com>
2024-08-16 13:05:49 +0530

    pyy.py

--- pyy.py
+++ pyy.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
 url = 'https://screener.in/company/RELIANCE/consolidated/'
 webpage = requests.get(url)
