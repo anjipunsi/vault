e7c30c38b8ab655375bfd3cfd160b1775d4cd640
Anjali punsi <108992453+anjipunsi@users.noreply.github.com>
2024-08-16 13:49:29 +0530

    Update pyy.py

--- pyy.py
+++ pyy.py
@@ -8,28 +8,34 @@ url = 'https://screener.in/company/RELIANCE/consolidated/'
 webpage = requests.get(url)
 soup = bs(webpage.text,'html.parser')
 data = soup.find('section', id="profit-loss")
-tdata= data.find("table")
-table_data = []
-for row in tdata.find_all('tr'):
-    row_data = []
-    for cell in row.find_all(['th','td']):
-        row_data.append(cell.text.strip())
-    table_data.append(row_data)
+if data is not None:
+    tdata = data.find("table")
+    if tdata is not None:
+        table_data = []
+        for row in tdata.find_all('tr'):
+            row_data = []
+            for cell in row.find_all(['th','td']):
+                row_data.append(cell.text.strip())
+            table_data.append(row_data)
 
-df_table = pd.DataFrame(table_data)
-df_table.iloc[0,0] = 'Section'
-df_table.columns = df_table.iloc[0]
-df_table = df_table.iloc[1:,:-2]
-for i in df_table.iloc[:,1:].columns:
-    df_table[i] = df_table[i].str.replace(',','').str.replace('%','/100').apply(eval)
-# df_table = df_table.set_index('')
-print(df_table)
+        df_table = pd.DataFrame(table_data)
+        df_table.iloc[0,0] = 'Section'
+        df_table.columns = df_table.iloc[0]
+        df_table = df_table.iloc[1:,:-2]
+        for i in df_table.iloc[:,1:].columns:
+            df_table[i] = df_table[i].str.replace(',','').str.replace('%','/100').apply(eval)
+        # df_table = df_table.set_index('')
+        print(df_table)
 
-db_host = "192.168.3.38" 
-db_name = "postgres"
-db_user = "scrap"
-db_password = "scrap" 
-db_port="5432"
-engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
-df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
-print("Data loaded to Postgres")
+        db_host = "192.168.3.38" 
+        db_name = "postgres"
+        db_user = "scrap"
+        db_password = "scrap" 
+        db_port="5432"
+        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+        df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
+        print("Data loaded to Postgres")
+    else:
+        print("No table found in the section")
+else:
+    print("No section with id 'profit-loss' found")
